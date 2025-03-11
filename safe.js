e30503cbee83d8b15ab32c2efb1131d5829c70ba
MarkHildegard <mark.krause09@hotmail.com>
2025-03-11 17:37:06 +0100

    Update safe.js

--- safe.js
+++ safe.js
@@ -1,1 +1,8 @@
+const users = {};  // Speichert Benutzerdaten
+const scripts = [  // Liste der verkauften Skripte
+    { name: "AutoAim.lua", price: 5 },
+    { name: "SpeedHack.lua", price: 8 },
+    { name: "WallHack.lua", price: 10 }
+];
 
+const purchases = {};  // Speichert, wer was gekauft hat

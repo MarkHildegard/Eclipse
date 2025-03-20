7bb2c7ebed1788d90791b32c18b42b4949113e49
MarkHildegard <mark.krause09@hotmail.com>
2025-03-20 14:41:31 +0100

    Delete safe.js

--- safe.js
+++
@@ -1,8 +0,0 @@
-const users = {};  // Speichert Benutzerdaten
-const scripts = [  // Liste der Minecraft-Skripte
-    { name: "Example1.sk", price: 10 },
-    { name: "Example2.sk", price: 5 },
-    { name: "Example3.sk", price: 8 }
-];
-
-const purchases = {};  // Speichert, wer was gekauft hat
